525de36a6fd3ff39fc4299e8e025ebe29a23a6d5
Ryo inaba <vtryo.work@gmail.com>
2017-08-31 11:42:19 +0900

    add import instance

--- terraform/aws/aws_ec2.tf
+++ terraform/aws/aws_ec2.tf
@@ -6,3 +6,9 @@ resource "aws_instance" "vtryo-web01" {
   subnet_id = "${aws_subnet.public-a.id}"
   vpc_security_group_ids = ["${aws_security_group.web.id}"]
 }
+
+## terraform import
+resource "aws_instance" "vtryo-manual" {
+  ami = "${var.ami}"
+  instance_type = "${var.instance_type_web}"
+}
